7b6174a8df9b845750cc3497d1fb91754bc56cc7
Manuel Jimenez <manuel.jimenez@fleetcor.com>
2026-05-27 10:43:52 -0400

    ♻️ refactor(functions): migrate from aws-sdk v2 to v3 and switch to pnpm
    - replace aws-sdk v2 with @aws-sdk/client-personalize-runtime v3 modular package
    - update dotenv from ^8.2.0 to ^17.4.2
    - migrate from PersonalizeRuntime to PersonalizeRuntimeClient with GetRecommendationsCommand
    - switch from yarn to pnpm package manager
    - update code to use send() pattern instead of .promise() callbacks

--- functions/rotalo-recommendations.js
+++ functions/rotalo-recommendations.js
@@ -1,34 +1,34 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const AWS = require("aws-sdk");
+const {
+  PersonalizeRuntimeClient,
+  GetRecommendationsCommand,
+} = require("@aws-sdk/client-personalize-runtime");
 
 const userIds = ["22372", "22453"];
 const numResults = 10;
 
-const personalize = new AWS.PersonalizeRuntime({
-    region: "us-east-1",
-    apiVersion: "2018-05-22"
-});
+const personalize = new PersonalizeRuntimeClient({ region: "us-east-1" });
 
-const promises = userIds.map(userId =>
-    personalize
-        .getRecommendations({
-            campaignArn: process.env.RECOMMENDATIONS_CAMPAIGN_ARN,
-            userId,
-            numResults
-        })
-        .promise()
+const promises = userIds.map((userId) =>
+  personalize.send(
+    new GetRecommendationsCommand({
+      campaignArn: process.env.RECOMMENDATIONS_CAMPAIGN_ARN,
+      userId,
+      numResults,
+    }),
+  ),
 );
 
 Promise.all(promises)
-    .then(recommendations =>
-        recommendations.map(({ itemList }, i) => {
-            return {
-                userId: userIds[i],
-                recommendations: itemList.map(({ itemId }) => itemId)
-            };
-        })
-    )
-    .then(response => {
-        console.log(JSON.stringify(response, null, 2));
-    });
+  .then((recommendations) =>
+    recommendations.map(({ itemList }, i) => {
+      return {
+        userId: userIds[i],
+        recommendations: itemList.map(({ itemId }) => itemId),
+      };
+    }),
+  )
+  .then((response) => {
+    console.log(JSON.stringify(response, null, 2));
+  });
